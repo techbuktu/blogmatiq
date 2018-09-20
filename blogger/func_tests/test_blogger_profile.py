90c356a23ca16a74d0f328ab728cfbdf2e11dca0
Muhammad <blm@techbuktu.com>
2018-09-20 18:32:50 -0400

    Created initial Functional Test class placeholders for the Blogger app

---
+++ blogger/func_tests/test_blogger_profile.py
@@ -0,0 +1,11 @@
+from functional_tests.base import BaseFunctionalTest 
+
+class NewBloggerTest(BaseFunctionalTest):
+    pass 
+
+class BloggerProfileTest(BaseFunctionalTest):
+    pass 
+
+class BloggerProfileUpdateTest(BaseFunctionalTest):
+    pass 
+
