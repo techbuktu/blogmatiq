4381020d7356e8251f74ef5d5eceadbf7a49d7a6
Muhammad <blm@techbuktu.com>
2018-09-20 18:09:33 -0400

    Setup the base Functional Test class

---
+++ functional_tests/base.py
@@ -0,0 +1,11 @@
+from selenium import webdriver 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase 
+
+class BaseFunctionalTest(StaticLiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    
