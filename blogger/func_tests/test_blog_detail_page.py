c3928621159ff46a7ab24a2a760ece910c8d5d02
Muhammad <blm@techbuktu.com>
2018-10-12 01:44:49 -0400

    Created placeholder classes for the test_blog_detail_page.py Functional Tests module

---
+++ blogger/func_tests/test_blog_detail_page.py
@@ -0,0 +1,26 @@
+from selenium import webdriver 
+from functional_tests.base import BaseFunctionalTest 
+from selenium.webdriver.common.keys import Keys 
+
+
+class BlogOwnerTest(BaseFunctionalTest):
+    """
+    Tests a User's interaction with the blog detail page.
+    """
+
+    def test_blog_has_link_to_owner(self):
+        pass 
+
+class BlogDetailTest(BaseFunctionalTest):
+    """
+    Test that a Blog has all the right attributes/components 
+    """
+    def test_blog_has_all_its_attributes(self):
+        pass 
+
+    def test_blog_detail_page_has_list_of_categories(self):
+        pass 
+
+    def test_blog_has_list_of_recent_blog_posts(self):
+        pass 
+
